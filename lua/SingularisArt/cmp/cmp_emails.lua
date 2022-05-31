808721f5bace976dc2f3237c4004b32101f7b86a
Hashem A. Damrah <dermdorm2@gmail.com>
2022-05-30 19:47:04 -0700

    improvement(snippets, cmp)
    Add math snippets for markdown because I'm starting to take notes in
    markdown as well as latex. I fixed the issue with the issues_cmp thing.
    Now, when you type `#`, you get a list of the open issues on the github
    repo. I also added a email_cmp where you type `@`, you get a list of
    emails. Cmp reads the emails from a file located
    `~/.github-handles.json`. A basic format is:
    
    ```
    {
      "hashem": "Hashem A. Damrah <dermdorm2@gmail.com>"
    }
    ```
    
    -------------------------------------------------------------------------------
    Hashem A. Damrah
    
    dermdorm2@gmail.com
    https://damrah.netlify.app
    -------------------------------------------------------------------------------

---
+++ lua/SingularisArt/cmp/cmp_emails.lua
@@ -0,0 +1,95 @@
+-- Custom nvim-cmp source for GitHub handles.
+
+local handles = {}
+
+local registered = false
+
+handles.setup = function()
+  if registered then
+    return
+  end
+  registered = true
+
+  local has_cmp, cmp = pcall(require, 'cmp')
+
+  if not has_cmp then
+    return
+  end
+
+  local config = vim.fn.expand('~/.github-handles.json')
+  if vim.fn.filereadable(config) == 0 then
+    return
+  end
+  local addresses = vim.fn.json_decode(vim.fn.readfile(config))
+
+  local source = {}
+
+  source.new = function()
+    return setmetatable({}, {__index = source})
+  end
+
+  source.get_trigger_characters = function()
+    return { '@' }
+  end
+
+  source.get_keyword_pattern = function()
+    -- Add dot to existing keyword characters (\k).
+    return [[\%(\k\|\.\)\+]]
+  end
+
+  source.complete = function(self, request, callback)
+    local input = string.sub(request.context.cursor_before_line, request.offset - 1)
+    local prefix = string.sub(request.context.cursor_before_line, 1, request.offset - 1)
+
+    if vim.startswith(input, '@') and (prefix == '@' or vim.endswith(prefix, ' @')) then
+      local items = {}
+      for handle, address in pairs(addresses) do
+        table.insert(items, {
+            filterText = handle .. ' ' .. address,
+            label = address,
+            textEdit = {
+              newText = address,
+              range = {
+                start = {
+                  line = request.context.cursor.row - 1,
+                  character = request.context.cursor.col - 1 - #input,
+                },
+                ['end'] = {
+                  line = request.context.cursor.row - 1,
+                  character = request.context.cursor.col - 1,
+                },
+              },
+            },
+          }
+        )
+      end
+      callback {
+        items = items,
+        isIncomplete = true,
+      }
+    else
+      callback({isIncomplete = true})
+    end
+  end
+
+  cmp.register_source('handles', source.new())
+
+  local filetypes = {'gitcommit', 'mail'}
+  cmp.setup.filetype(filetypes, {
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "nvim_lua" },
+      { name = "latex_symbols" },
+      { name = "ultisnips" },
+      { name = "calc" },
+      { name = "path" },
+      { name = "buffer" },
+      { name = "gh_issues" },
+
+      -- My custom sources.
+      { name = 'handles' }, -- GitHub handles; eg. @hashem → Hashem A. Damrah <singularis@github.com>
+    }),
+  })
+end
+
+return handles
---
+++ lua/SingularisArt/cmp/init.lua
@@ -0,0 +1,3 @@
+require('SingularisArt.cmp.cmp')
+require('SingularisArt.cmp.cmp_github_issues')
+require('SingularisArt.cmp.cmp_emails').setup()
