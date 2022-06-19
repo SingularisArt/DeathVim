7c090b7dbe81da4bbd0054150fcf6b2c2e90153d
SingularisArt <dermdorm2@gmail.com>
2022-06-18 19:23:17 -0700

    update(clean up lua/SingularisArt/autocommands.lua)

--- lua/SingularisArt/autocommands.lua
+++ lua/SingularisArt/autocommands.lua
@@ -1,45 +1,80 @@
-vim.cmd [[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-  augroup end
+------------------------------------------------------------------------
+--                         FileType AutoCmds                          --
+------------------------------------------------------------------------
 
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
+-- Enable wrap and spell for markdown, git, and tex filetypes
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown", "git", "tex" },
+  callback = function()
+    vim.opt.wrap=true
+    vim.opt.spell=true
+  end,
+})
 
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
+------------------------------------------------------------------------
+--                          Buffer AutoCmds                           --
+------------------------------------------------------------------------
 
-  augroup _tex
-    autocmd!
-    autocmd FileType tex setlocal showtabline=0
-    autocmd FileType tex setlocal laststatus=0
-  augroup end
+-- Something to do with NvimTree, I just don't know what...
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  callback = function()
+    vim.cmd [[
+      if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+      normal zz
+    ]]
+  end,
+})
 
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd = 
-  augroup end
+-- When in a tex file, remove statusbar and tabline, but in other filetypes,
+-- enable them
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  callback = function()
+    if vim.bo.filetype == 'tex' then
+      vim.opt.showtabline=0
+      vim.opt.laststatus=0
+    else
+      vim.opt.showtabline=2
+      vim.opt.laststatus=2
+    end
+  end,
+})
 
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
+------------------------------------------------------------------------
+--                           User AutoCmds                            --
+------------------------------------------------------------------------
 
-  augroup _jump_to_last_line
-    autocmd!
-    autocmd BufReadPost * 
+-- Removes statusbar and tabline from Alpha
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = { "AlphaReady" },
+  callback = function()
+    vim.cmd [[
+      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+    ]]
+  end,
+})
+
+------------------------------------------------------------------------
+--                           Other AutoCmds                           --
+------------------------------------------------------------------------
+
+-- I honestly don't know what this does...
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd "tabdo wincmd ="
+  end,
+})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 100 }
+  end,
+})
+
+-- Always go back to the line you quit on
+vim.cmd[[
+autocmd BufReadPost *
      \ if line("'\"") > 0 && line("'\"") <= line("$") |
      \   exe "normal! g`\"" |
      \ endif
-  augroup end
 ]]
