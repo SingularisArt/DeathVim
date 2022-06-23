1e003e1e78ef39ea8fc06b300c92fe077c1c54a9
SingularisArt <dermdorm2@gmail.com>
2022-06-23 16:23:29 -0700

    fix(fix some lsp issues with diagnostics and formaters)

--- lua/dvim/lsp/init.lua
+++ lua/dvim/lsp/init.lua
@@ -8,3 +8,6 @@ Utils.safe_require("lspconfig")
 Utils.safe_require("dvim.lsp.servers")
 Utils.safe_require("dvim.lsp.lsp-signature")
 Utils.safe_require("dvim.lsp.handlers").setup()
+
+Utils.safe_require("dvim.lsp.null-ls.diagnostics")
+Utils.safe_require("dvim.lsp.null-ls.formatters")
