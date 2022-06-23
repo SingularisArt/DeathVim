2fb8ffb7f6a0fd77c7c41408883780d408975494
SingularisArt <dermdorm2@gmail.com>
2022-06-23 16:00:04 -0700

    fix(fix installation issue)

--- lua/dvim/utils/functions.lua
+++ lua/dvim/utils/functions.lua
@@ -15,7 +15,6 @@ vim.cmd [[
 function M.safe_require(plugin_name)
   local status_ok, plugin = pcall(require, plugin_name)
   if not status_ok then
-    print('Cannot source ' .. plugin_name)
     return
   end
 
