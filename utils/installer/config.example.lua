aef77736ae6be3454f4ebca6bbdecdc4a5cfa063
SingularisArt <dermdorm2@gmail.com>
2022-06-24 17:50:20 -0700

    update(update example.config.lua)

--- utils/installer/config.example.lua
+++ utils/installer/config.example.lua
@@ -27,35 +27,90 @@ dvim.leader = " "
 --------------------------
 
 ------------------------------------------------------------------------
---                           Builtin Plugins                          --
+--                              Builtin                               --
 ------------------------------------------------------------------------
+
+-----------------------
+--  Builtin Plugins  --
+-----------------------
 
 -- After changing plugin config exit and reopen DeathVim, then
 -- Run :PackerSync
-dvim.builtin.alpha.active = true
-dvim.builtin.alpha.mode = "dashboard"
-dvim.builtin.autopairs.active = true
-dvim.builtin.bufferline.active = true
-dvim.builtin.cmp.active = true
-dvim.builtin.dap.active = true
-dvim.builtin.symbols_outline.active = true
-dvim.builtin.todo_comments.active = true
-dvim.builtin.gitsigns.active = true
-dvim.builtin.comment.active = true
-dvim.builtin.jabs.active = true
-dvim.builtin.surround.active = true
-dvim.builtin.comments.active = true
-dvim.builtin.lualine.active = true
-dvim.builtin.markdown.active = true
-dvim.builtin.nvimtree.active = true
-dvim.builtin.telescope.active = true
-dvim.builtin.project.active = true
-dvim.builtin.terminal.active = true
-dvim.builtin.ultisnips.active = true
-dvim.builtin.vimtex.active = true
-dvim.builtin.which_key.active = true
-dvim.builtin.winbar.active = true
-dvim.builtin.notify.active = true
+dvim.builtin.plugins.alpha.active = true
+dvim.builtin.plugins.alpha.mode = "dashboard"
+dvim.builtin.plugins.indent_blankline.active = true
+dvim.builtin.plugins.symbols_outline.active = true
+dvim.builtin.plugins.which_key.active = true
+dvim.builtin.plugins.gitsigns.active = true
+dvim.builtin.plugins.comment.active = true
+dvim.builtin.plugins.surround.active = true
+dvim.builtin.plugins.todo_comments.active = true
+dvim.builtin.plugins.jabs.active = true
+dvim.builtin.plugins.terminal.active = true
+dvim.builtin.plugins.dap.active = true
+dvim.builtin.plugins.alpha.active = true
+dvim.builtin.plugins.ultisnips.active = true
+dvim.builtin.plugins.telescope.active = true
+dvim.builtin.plugins.project.active = true
+dvim.builtin.plugins.copilot.active = true
+dvim.builtin.plugins.cmp.active = true
+dvim.builtin.plugins.autopairs.active = true
+dvim.builtin.plugins.nvimtree.active = true
+dvim.builtin.plugins.lualine.active = true
+dvim.builtin.plugins.bufferline.active = true
+dvim.builtin.plugins.notify.active = true
+dvim.builtin.plugins.treesitter.active = true
+dvim.builtin.plugins.latex.active = true
+dvim.builtin.plugins.markdown.active = true
+
+-------------------------
+--  Builtin Filetypes  --
+-------------------------
+
+-- TODO: Add configuration for each of these filetypes
+-- Also, I need to add way more than this
+
+dvim.builtin.filetypes.python.active = true
+dvim.builtin.filetypes.cpp.active = true
+dvim.builtin.filetypes.cs.active = true
+dvim.builtin.filetypes.c.active = true
+dvim.builtin.filetypes.sql.active = true
+dvim.builtin.filetypes.html.active = true
+dvim.builtin.filetypes.css.active = true
+dvim.builtin.filetypes.js.active = true
+dvim.builtin.filetypes.ts.active = true
+dvim.builtin.filetypes.php.active = true
+dvim.builtin.filetypes.ruby.active = true
+dvim.builtin.filetypes.perl.active = true
+dvim.builtin.filetypes.java.active = true
+dvim.builtin.filetypes.rust.active = true
+dvim.builtin.filetypes.latex.active = true
+dvim.builtin.filetypes.markdown.active = true
+dvim.builtin.filetypes.lua.active = true
+dvim.builtin.filetypes.vim.active = true
+
+------------------------------------------------------------------------
+--                            Tree Sitter                             --
+------------------------------------------------------------------------
+
+-- if you don't want all the parsers change this to a table of the ones you want
+-- dvim.builtin.plugins.treesitter.ensure_installed = {
+--   "bash",
+--   "c",
+--   "javascript",
+--   "json",
+--   "lua",
+--   "python",
+--   "typescript",
+--   "tsx",
+--   "css",
+--   "rust",
+--   "java",
+--   "yaml",
+-- }
+
+-- dvim.builtin.plugins.treesitter.ignore_install = { "haskell" }
+-- dvim.builtin.plugins.treesitter.highlight.enabled = true
 
 ------------------------------------------------------------------------
 --                                LSP                                 --
@@ -144,35 +199,35 @@ dvim.builtin.notify.active = true
 -- vim.api.nvim_create_autocmd("CursorMoved", {
 --   callback = function()
 --     if vim.bo.filetype ~= "tex" then
---       require("dvim.core.winbar").setup()
+--       require("dvim.core.plugins.winbar").setup()
 --     end
 --   end
 -- })
 -- vim.api.nvim_create_autocmd("BufWinEnter", {
 --   callback = function()
 --     if vim.bo.filetype ~= "tex" then
---       require("dvim.core.winbar").setup()
+--       require("dvim.core.plugins.winbar").setup()
 --     end
 --   end
 -- })
 -- vim.api.nvim_create_autocmd("BufFilePost", {
 --   callback = function()
 --     if vim.bo.filetype ~= "tex" then
---       require("dvim.core.winbar").setup()
+--       require("dvim.core.plugins.winbar").setup()
 --     end
 --   end
 -- })
 -- vim.api.nvim_create_autocmd("InsertEnter", {
 --   callback = function()
 --     if vim.bo.filetype ~= "tex" then
---       require("dvim.core.winbar").setup()
+--       require("dvim.core.plugins.winbar").setup()
 --     end
 --   end
 -- })
 -- vim.api.nvim_create_autocmd("BufWritePost", {
 --   callback = function()
 --     if vim.bo.filetype ~= "tex" then
---       require("dvim.core.winbar").setup()
+--       require("dvim.core.plugins.winbar").setup()
 --     end
 --   end
 -- })
