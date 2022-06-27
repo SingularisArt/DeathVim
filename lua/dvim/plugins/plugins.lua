b350b80ad194af2f6cd057fcf478e2691ff9e8b6
SingularisArt <dermdorm2@gmail.com>
2022-06-27 01:38:14 -0700

    add(add more screenshots)

--- lua/dvim/plugins/plugins.lua
+++ lua/dvim/plugins/plugins.lua
@@ -16,12 +16,12 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- vim.cmd([[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -71,6 +71,7 @@ return require("packer").startup(function(use)
 	use({ "folke/tokyonight.nvim" })
 	use({ "chriskempson/base16-vim" })
 	use({ "flazz/vim-colorschemes" })
+	use({ "rose-pine/neovim" })
 
 	-- Indent Blankline
 	use({
