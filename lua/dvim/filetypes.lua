4e39509ab31eba11bd25ea16c7d348e54c297bc6
SingularisArt <dermdorm2@gmail.com>
2022-07-22 13:00:41 -0700

    update latex snippets and filetype configurations

--- lua/dvim/filetypes.lua
+++ lua/dvim/filetypes.lua
@@ -1,23 +1,22 @@
 local M = {}
 
 M.filetype_settings = function()
-  for filetype, filetype_table in pairs(dvim.builtin.filetypes) do
-    for command, value in pairs(filetype_table) do
-      if command == "active" then
-        if value == true then
-          vim.api.nvim_create_autocmd("BufEnter", {
-            pattern = "*",
-            callback = function()
-              if vim.bo.filetype == filetype then
-                require("dvim.core.filetypes." .. filetype)
-                pcall(require, "dvim.core.filetypes." .. filetype)
-              end
-            end,
-          })
-        end
-      end
-    end
-  end
+	for filetype, filetype_table in pairs(dvim.builtin.filetypes) do
+		for command, value in pairs(filetype_table) do
+			if command == "active" then
+				if value == true then
+					vim.api.nvim_create_autocmd("BufEnter", {
+						pattern = "*",
+						callback = function()
+							if vim.bo.filetype == filetype then
+								pcall(require, "dvim.core.filetypes." .. filetype)
+							end
+						end,
+					})
+				end
+			end
+		end
+	end
 end
 
 return M
