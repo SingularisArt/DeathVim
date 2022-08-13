04977cbb2d5f33a600393e4d5ae2ef8f44911ef1
Hashem A. Damrah <dermdorm2@gmail.com>
2022-08-12 18:54:05 -0700

    fix python issue and add support for sioyek

--- lua/dvim/core/plugins/vimtex.lua
+++ lua/dvim/core/plugins/vimtex.lua
@@ -1,12 +1,14 @@
 local M = {}
 
 M.setup = function()
-  vim.cmd [[
+  vim.cmd([[
     "set conceallevel=2
-    let g:vimtex_view_method='zathura'
+    let g:vimtex_view_method = 'sioyek'
+    let g:latex_view_general_viewer = 'sioyek'
+    let g:vimtex_compiler_progname = 'nvr'
     let g:tex_flavor='latex'
     let g:vimtex_quickfix_enabled=0
-  ]]
+  ]])
 end
 
 return M
--- lua/dvim/utils/functions.lua
+++ lua/dvim/utils/functions.lua
@@ -1,53 +1,51 @@
 local M = {}
 
-vim.cmd [[
-  function Test()
-    %SnipRun
-    call feedkeys("\<esc>`.")
-  endfunction
-  function TestI()
-    let b:caret = winsaveview()    
-    %SnipRun
-    call winrestview(b:caret)
-  endfunction
-]]
+-- vim.cmd [[
+--   function Test()
+--     %SnipRun
+--     call feedkeys("\<esc>`.")
+--   endfunction
+--   function TestI()
+--     let b:caret = winsaveview()    
+--     %SnipRun
+--     call winrestview(b:caret)
+--   endfunction
+-- ]]
 
-function M.sniprun_enable()
-  vim.cmd [[
-    %SnipRun
-    augroup _sniprun
-     autocmd!
-     autocmd TextChanged * call Test()
-     autocmd TextChangedI * call TestI()
-    augroup end
-  ]]
-  vim.notify "Enabled SnipRun"
-end
+-- function M.sniprun_enable()
+--   vim.cmd [[
+--     %SnipRun
+--     augroup _sniprun
+--      autocmd!
+--      autocmd TextChanged * call Test()
+--      autocmd TextChangedI * call TestI()
+--     augroup end
+--   ]]
+--   vim.notify "Enabled SnipRun"
+-- end
 
-function M.disable_sniprun()
-  M.remove_augroup "_sniprun"
-  vim.cmd [[
-    SnipClose
-    SnipTerminate
-    ]]
-  vim.notify "Disabled SnipRun"
-end
+-- function M.disable_sniprun()
+--   M.remove_augroup "_sniprun"
+--   vim.cmd [[
+--     SnipClose
+--     SnipTerminate
+--     ]]
+--   vim.notify "Disabled SnipRun"
+-- end
 
-function M.toggle_sniprun()
-  if vim.fn.exists "#_sniprun#TextChanged" == 0 then
-    M.sniprun_enable()
-  else
-    M.disable_sniprun()
-  end
-end
+-- function M.toggle_sniprun()
+--   if vim.fn.exists "#_sniprun#TextChanged" == 0 then
+--     M.sniprun_enable()
+--   else
+--     M.disable_sniprun()
+--   end
+-- end
 
 function M.remove_augroup(name)
   if vim.fn.exists("#" .. name) == 1 then
     vim.cmd("au! " .. name)
   end
 end
-
-vim.cmd [[ command! SnipRunToggle execute 'lua require("dvim.utils.functions").toggle_sniprun()' ]]
 
 -- get length of current word
 function M.get_word_length()
