e837ab0a19cf69d3ecd292f28be6d9b9a6529634
Hashem A. Damrah <dermdorm2@gmail.com>
2022-08-14 06:52:50 -0700

    fix ultisnips typo

--- lua/dvim/core/plugins/ultisnips.lua
+++ lua/dvim/core/plugins/ultisnips.lua
@@ -8,7 +8,6 @@ M.setup = function()
     let g:SuperTabDefaultCompletionType = '<C-n>'
 
     " better key bindings for UltiSnipsExpandTrigger
-    let g:UltiSnipsExpandTrigger = "<C-Enter>"
     let g:UltiSnipsJumpForwardTrigger = "<C-j>"
     let g:UltiSnipsJumpBackwardTrigger = "<C-k>"
 
