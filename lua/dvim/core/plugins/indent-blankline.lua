18f2ea2421910bc69c1c6a33ea4327d6e5868303
Hashem A. Damrah <singularisartt@gmail.com>
2022-08-30 05:01:05 -0700

    update lua/dvim/core/plugins/indent-blankline.lua

--- lua/dvim/core/plugins/indent-blankline.lua
+++ lua/dvim/core/plugins/indent-blankline.lua
@@ -5,7 +5,7 @@ M.config = function()
     active = true,
     setup = {
       show_current_context = true,
-    }
+    },
   }
 
   vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
@@ -19,9 +19,7 @@ M.config = function()
     "Trouble",
   }
   vim.g.indentLine_enabled = 1
-  -- vim.g.indent_blankline_char = "│"
   vim.g.indent_blankline_char = "▏"
-  -- vim.g.indent_blankline_char = "▎"
   vim.g.indent_blankline_show_trailing_blankline_indent = false
   vim.g.indent_blankline_show_first_indent_level = true
   vim.g.indent_blankline_use_treesitter = true
